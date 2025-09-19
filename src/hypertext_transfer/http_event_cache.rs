e0d75e7a94c162208abe436ba1938e5098e26549
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-09-19 04:46:41 -0600

    Refactor

--- src/hypertext_transfer/http_event_cache.rs
+++ src/hypertext_transfer/http_event_cache.rs
@@ -1,10 +1,10 @@
 #![allow(dead_code)]
 
-use std::{collections::HashMap, result::Result, string::String};
+use std::{collections::HashMap, string::String};
 
-use crate::{errors::http_error::HttpError, hypertext_transfer::http_request::HttpRequest};
+use crate::hypertext_transfer::http_request::HttpRequest;
 
 // Hypertext Transfer Protocol Event Result Cache Definition
 pub struct HttpEventResultCache {
-    pub cache: HashMap<String, Result<HttpRequest, HttpError>>,
+    pub cache: HashMap<String, HttpRequest>,
 }
--- src/hypertext_transfer/http_task_cache.rs
+++ src/hypertext_transfer/http_task_cache.rs
@@ -1,10 +1,10 @@
 #![allow(dead_code)]
 
-use std::{collections::HashMap, result::Result, string::String};
+use std::{collections::HashMap, string::String};
 
-use crate::{errors::http_error::HttpError, hypertext_transfer::http_response::HttpResponse};
+use crate::hypertext_transfer::http_response::HttpResponse;
 
 // Hypertext Transfer Protocol Task Result Cache Definition
 pub struct HttpTaskResultCache {
-    pub cache: HashMap<String, Result<HttpResponse, HttpError>>,
+    pub cache: HashMap<String, HttpResponse>,
 }
