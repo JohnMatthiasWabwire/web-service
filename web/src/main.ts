0517b533676fff5701799b171275d9afc4350c67
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-10-01 07:20:58 -0600

    Intermediate Commit

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,27 +1,14 @@
 // Main Entrypoint
-function main(): HTMLHtmlElement {
-  const HTML_DOCUMENT_BODY: HTMLBodyElement = document.createElement("body");
-  const HTML_DOCUMENT_METADATA_HEADER: HTMLHeadElement = document.createElement("head");    
+function main(): HTMLElement {
   const HTML_MAIN: HTMLElement = document.createElement("main");
-  const HTML_METADATA: HTMLMetaElement = document.createElement("meta");
   const HTML_PARAGRAPH: HTMLParagraphElement = document.createElement("p");
-    
-  HTML_DOCUMENT_METADATA_HEADER  
-  HTML_METADATA
-  HTML_METADATA.name = String("main-index");
-  HTML_METADATA.content = String("width=device-width");
-  HTML_DOCUMENT_METADATA_HEADER.appendChild(HTML_METADATA);
-
-  document.appendChild(HTML_DOCUMENT_METADATA_HEADER);
   
-  HTML_DOCUMENT_BODY  
   HTML_MAIN
   HTML_PARAGRAPH
   HTML_PARAGRAPH.innerText = String("Hyaena Technologies");
   HTML_MAIN.appendChild(HTML_PARAGRAPH);
-  HTML_DOCUMENT_BODY.appendChild(HTML_MAIN);
 
-  document.appendChild(HTML_DOCUMENT_BODY); 
+  return document.body.appendChild(HTML_MAIN); 
 }
 
 main();
