20247abe3a1475e191416023af6ded8f2979159f
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-10-04 16:23:12 -0600

    TODO: Integrate Hyper, Build Client

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,14 +1,6 @@
 // Main Entrypoint
-function main(): HTMLElement {
-  const HTML_MAIN: HTMLElement = document.createElement("main");
-  const HTML_PARAGRAPH: HTMLParagraphElement = document.createElement("p");
+function main() {
   
-  HTML_MAIN
-  HTML_PARAGRAPH
-  HTML_PARAGRAPH.innerText = String("Hyaena Technologies");
-  HTML_MAIN.appendChild(HTML_PARAGRAPH);
-
-  return document.body.appendChild(HTML_MAIN); 
 }
 
 main();
---
+++ web/src/widgets/navigation-menu.ts
@@ -0,0 +1,19 @@
+export {
+  naviagtionMenu
+}
+
+// Navigation Menu
+function naviagtionMenu(): HTMLElement {
+  const HTML_MAIN: HTMLElement = document.createElement("main");
+  const HTML_PARAGRAPH: HTMLParagraphElement = document.createElement("p");
+  
+  HTML_MAIN
+  HTML_PARAGRAPH
+  HTML_PARAGRAPH.innerText = String("Hyaena Technologies");
+  HTML_MAIN.appendChild(HTML_PARAGRAPH);
+
+  return document.body.appendChild(HTML_MAIN); 
+}
+
+naviagtionMenu();
+
