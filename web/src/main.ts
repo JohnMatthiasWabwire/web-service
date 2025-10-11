8f1a2714e3c381f739febb48b20e03285fc1a3fd
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-10-10 19:04:11 -0600

    TODO: Integrate Hyper, JSON, YAML, Client

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,8 @@
+import { naviagtionMenu } from "./widgets/navigation-menu.js";
+
 // Main Entrypoint
 function main() {
-  
+  naviagtionMenu();
 }
 
 main();
--- web/src/widgets/navigation-menu.ts
+++ web/src/widgets/navigation-menu.ts
@@ -1,16 +1,17 @@
 export {
   naviagtionMenu
-}
+};
 
 // Navigation Menu
 function naviagtionMenu(): HTMLElement {
+  const HTML_HEADING_ONE: HTMLHeadElement = document.createElement("h1");
+  const HTML_HEADING_TWO: HTMLHeadingElement = document.createElement("h2");
   const HTML_MAIN: HTMLElement = document.createElement("main");
-  const HTML_PARAGRAPH: HTMLParagraphElement = document.createElement("p");
-  
+
   HTML_MAIN
-  HTML_PARAGRAPH
-  HTML_PARAGRAPH.innerText = String("Hyaena Technologies");
-  HTML_MAIN.appendChild(HTML_PARAGRAPH);
+  HTML_HEADING_ONE
+  HTML_HEADING_ONE.innerText = String("Hyaena Technologies");
+  HTML_MAIN.appendChild(HTML_HEADING_ONE);
 
   return document.body.appendChild(HTML_MAIN); 
 }
